3f2bf04439dbc02b884871fb3c40681ff5430c50
Spencer Clayton <tsclayton35@gmail.com>
2018-12-17 10:07:32 -0500

    2018 - day 14

--- 2018/day14.rb
+++ 2018/day14.rb
@@ -1,132 +1,90 @@
 #!/usr/bin/env ruby
 
-# Code from day10.rb with slight tweaks
-def generate_sparse_hash(numbers, lengths, iterations)
-  skip_size = 0
-  curr_index = 0
+def get_next_ten_recipes(num_initial_recipes)
+  recipes = [3, 7]
+  final_recipes = []
 
-  (1..iterations).each do |iteration|
-    lengths.each do |length|
-      start_index = curr_index
-      end_index = curr_index + length - 1
+  elf_1_index = 0
+  elf_2_index = 1
 
-      while start_index < end_index
-        # swap numbers
-        temp = numbers[start_index % numbers.length]
-        numbers[start_index % numbers.length] = numbers[end_index % numbers.length]
-        numbers[end_index % numbers.length] = temp
+  while recipes.length < (num_initial_recipes + 10)
+    recipe_sum = recipes[elf_1_index] + recipes[elf_2_index]
+    recipe_sum_string = recipe_sum.to_s
 
-        start_index += 1
-        end_index -= 1
+    for j in 0...recipe_sum_string.length
+      new_recipe = recipe_sum_string[j].to_i
+      recipes << new_recipe
+
+      break if recipes.length >= (num_initial_recipes + 10)
+    end
+
+    elf_1_index = (elf_1_index + recipes[elf_1_index] + 1) % recipes.length
+    elf_2_index = (elf_2_index + recipes[elf_2_index] + 1) % recipes.length
+  end
+
+  recipes[(recipes.length - 10)...recipes.length].join('')
+end
+
+def get_num_recipes_before_match(recipe_sequence)
+  recipes = [3, 7]
+  final_recipes = []
+
+  elf_1_index = 0
+  elf_2_index = 1
+
+  sequence_string = recipe_sequence.to_s
+  starting_match_index = 0
+  current_match_index = 0
+
+  while current_match_index < sequence_string.length
+    recipe_sum = recipes[elf_1_index] + recipes[elf_2_index]
+    recipe_sum_string = recipe_sum.to_s
+
+    for j in 0...recipe_sum_string.length
+      new_recipe = recipe_sum_string[j].to_i
+      recipes << new_recipe
+
+      if sequence_string[current_match_index].to_i == new_recipe
+        current_match_index += 1
+      else
+        current_match_index = 0
       end
 
-      curr_index = (curr_index + length + skip_size) % numbers.length
-
-      skip_size += 1
+      break if current_match_index >= sequence_string.length
     end
+
+    elf_1_index = (elf_1_index + recipes[elf_1_index] + 1) % recipes.length
+    elf_2_index = (elf_2_index + recipes[elf_2_index] + 1) % recipes.length
   end
 
-  numbers
-end
-
-def generate_dense_hash(sparse_hash)
-  sparse_hash.each_slice(16).map {|partition| partition.reduce(&:^)}
-end
-
-# Might as well convert right to binary instead of converting to hex first
-def convert_to_binary_string(dense_hash)
-  dense_hash.map do |part|
-    binary_part = part.to_s(2)
-    # append leading 0s if necessary
-    leading_zeros = '0' * (8 - binary_part.length)
-    leading_zeros + binary_part
-  end.join('')
-end
-
-def knot_hash(input)
-  ascii_input = input.each_char.map(&:ord)
-  numbers = (0..255).to_a
-  generate_sparse_hash(numbers, ascii_input + [17, 31, 73, 47, 23], 64)
-  dense_hash = generate_dense_hash(numbers)
-  convert_to_binary_string(dense_hash)
-end
-
-def generate_grid(input)
-  grid = []
-
-  for i in 0..127
-    kh = knot_hash("#{input}-#{i}")
-    grid[i] = kh.each_char.map {|char| char == '1'}
-  end
-
-  grid
-end
-
-def num_used_squares(input)
-  grid = generate_grid(input)
-
-  used_count = 0
-
-  grid.each do |row|
-    row.each do |column|
-      used_count += 1 if column
-    end
-  end
-
-  used_count
-end
-
-def mark_regions(grid, regions, row, col)
-  # return if cell not used or already marked
-  return unless grid[row][col] && !regions[row][col]
-
-  regions[row][col] = true
-
-  # Left
-  mark_regions(grid, regions, row - 1, col) if (row - 1) >= 0
-  # Right
-  mark_regions(grid, regions, row + 1, col) if (row + 1) < grid.length
-  # Up
-  mark_regions(grid, regions, row, col - 1) if (col - 1) >= 0
-  # Down
-  mark_regions(grid, regions, row, col + 1) if (col + 1) < grid[row].length
-end
-
-def num_regions(input)
-  grid = generate_grid(input)
-  regions = []
-
-  region_count = 0
-
-  for row in 0...grid.length do
-    regions[row] = Array.new(grid[row].length, false)
-  end
-
-  for row in 0...grid.length do
-    for col in 0...grid[row].length do
-      if grid[row][col] && !regions[row][col]
-        # new region, increment counter and recursively mark all adjacent cells
-        region_count += 1
-        mark_regions(grid, regions, row, col)
-      end
-    end
-  end
-
-  region_count
+  recipes.length - sequence_string.length
 end
 
 def part_1
   puts("EXAMPLE SOLUTION:")
-  puts(num_used_squares('flqrgnkx'))
+  puts(get_next_ten_recipes(9))
+  puts(get_next_ten_recipes(5))
+  puts(get_next_ten_recipes(18))
+  puts(get_next_ten_recipes(2018))
+
   puts("INPUT SOLUTION:")
-  puts(num_used_squares('wenycdww'))
+  puts(get_next_ten_recipes(768071))
 end
 
 def part_2
   puts("EXAMPLE SOLUTION:")
-  puts(num_regions('flqrgnkx'))
+  puts(get_num_recipes_before_match(51589))
+  # 9
+  puts(get_num_recipes_before_match(01245))
+  # 5
+  puts(get_num_recipes_before_match(92510))
+  # 18
+  puts(get_num_recipes_before_match(59414))
+  # 2018
+
   puts("INPUT SOLUTION:")
-  puts(num_regions('wenycdww'))
+  puts(get_num_recipes_before_match(768071))
+  # 20198090
 end
 
 puts("PART 1 SOLUTIONS:")
