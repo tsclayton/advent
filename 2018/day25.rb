ef648c79d2ddb4ddb1a7e679ce2a4123686e0bb3
Spencer Clayton <tsclayton35@gmail.com>
2018-12-25 14:35:26 -0500

    2018 - day 25

--- 2018/day25.rb
+++ 2018/day25.rb
@@ -1,86 +1,130 @@
 #!/usr/bin/env ruby
 
-class BitRule
-  attr_accessor :bit_to_write
-  attr_accessor :slot_increment
-  attr_accessor :next_state
+class Point
+  attr_accessor :coords
+  attr_accessor :neighbours
 
-  def initialize(bit_to_write, slot_increment, next_state)
-    @slot_increment = slot_increment
-    @bit_to_write = bit_to_write
-    @next_state = next_state
+  def initialize(coords)
+    @coords = coords
+    @neighbours = []
   end
 end
 
-class TuringMachine
-  attr_accessor :rules
-  attr_accessor :num_steps
+def get_points_from_file(filename)
+  points = []
 
-  attr_accessor :curr_state
-  attr_accessor :curr_index
-
-  attr_accessor :tape
-
-  def initialize(rules, num_steps, starting_state)
-    @rules = rules
-    @num_steps = num_steps
-    @curr_state = starting_state
-    @curr_index = 0
-    @tape = {}
-  end
-
-  def checksum()
-    @tape.values.reduce(:+)
-  end
-
-  def run()
-    for i in 0...@num_steps
-      @tape[curr_index] ||= 0
-      bit_rule = @rules[@curr_state][@tape[curr_index]]
-      @tape[curr_index] = bit_rule.bit_to_write
-      @curr_index += bit_rule.slot_increment
-      @curr_state = bit_rule.next_state
+  File.open(filename, "r") do |file|
+    file.each_line do |line|
+      points << Point.new(line.scan(/-?[0-9]+/).map(&:to_i))
     end
   end
+
+  points
 end
 
-def get_example_machine()
-  rules = {
-    'A' => [BitRule.new(1, 1, 'B'), BitRule.new(0, -1, 'B')],
-    'B' => [BitRule.new(1, -1, 'A'), BitRule.new(1, 1, 'A')]
-  }
-  TuringMachine.new(rules, 6, 'A')
+def distance(point_a, point_b)
+  distance = 0
+
+  for i in 0...point_a.coords.length
+    distance += (point_a.coords[i] - point_b.coords[i]).abs
+  end
+
+  distance
 end
 
-def get_input_machine()
-  rules = {
-    'A' => [BitRule.new(1, 1, 'B'), BitRule.new(0, 1, 'C')],
-    'B' => [BitRule.new(0, -1, 'A'), BitRule.new(0, 1, 'D')],
-    'C' => [BitRule.new(1, 1, 'D'), BitRule.new(1, 1, 'A')],
-    'D' => [BitRule.new(1, -1, 'E'), BitRule.new(0, -1, 'D')],
-    'E' => [BitRule.new(1, 1, 'F'), BitRule.new(1, -1, 'B')],
-    'F' => [BitRule.new(1, 1, 'A'), BitRule.new(1, 1, 'E')]
-  }
-  TuringMachine.new(rules, 12368930, 'A')
+def traverse_constellation(point, hit_points)
+  return if !hit_points[point.coords.join(',')].nil?
+  hit_points[point.coords.join(',')] = true
+
+  point.neighbours.each do |neighbour|
+    traverse_constellation(neighbour, hit_points)
+  end
 end
 
-def run_diagnostic_checksum(machine)
-  machine.run()
-  machine.checksum()
+def get_num_constellations(points)
+  points.each_with_index do |point_a, i|
+    points.each_with_index do |point_b, j|
+      next if i == j
+      if distance(point_a, point_b) <= 3
+        point_a.neighbours << point_b
+        point_b.neighbours << point_a
+      end
+    end
+  end
+
+  num_constellations = 0
+  hit_points = {}
+
+  points.each do |point|
+    next if !hit_points[point.coords.join(',')].nil?
+    traverse_constellation(point, hit_points)
+    num_constellations += 1
+  end
+
+  num_constellations
 end
 
 def part_1
-  puts("EXAMPLE SOLUTION:")
-  puts(run_diagnostic_checksum(get_example_machine()))
-  puts("INPUT SOLUTION:")
-  puts(run_diagnostic_checksum(get_input_machine()))
-end
+  puts("EXAMPLE SOLUTIONS:")
+  example_1_points = [
+    Point.new([0,0,0,0]),
+    Point.new([3,0,0,0]),
+    Point.new([0,3,0,0]),
+    Point.new([0,0,3,0]),
+    Point.new([0,0,0,3]),
+    Point.new([0,0,0,6]),
+    Point.new([9,0,0,0]),
+    Point.new([12,0,0,0])
+  ]
+  puts(get_num_constellations(example_1_points))
 
-def part_2
-  puts("Nothing, apparently")
+  example_2_points = [
+    Point.new([-1,2,2,0]),
+    Point.new([0,0,2,-2]),
+    Point.new([0,0,0,-2]),
+    Point.new([-1,2,0,0]),
+    Point.new([-2,-2,-2,2]),
+    Point.new([3,0,2,-1]),
+    Point.new([-1,3,2,2]),
+    Point.new([-1,0,-1,0]),
+    Point.new([0,2,1,-2]),
+    Point.new([3,0,0,0]),
+  ]
+  puts(get_num_constellations(example_2_points))
+
+  example_3_points = [
+    Point.new([1,-1,0,1]),
+    Point.new([2,0,-1,0]),
+    Point.new([3,2,-1,0]),
+    Point.new([0,0,3,1]),
+    Point.new([0,0,-1,-1]),
+    Point.new([2,3,-2,0]),
+    Point.new([-2,2,0,0]),
+    Point.new([2,-2,0,-1]),
+    Point.new([1,-1,0,-1]),
+    Point.new([3,2,0,2])
+  ]
+  puts(get_num_constellations(example_3_points))
+
+  example_4_points = [
+    Point.new([1,-1,-1,-2]),
+    Point.new([-2,-2,0,1]),
+    Point.new([0,2,1,3]),
+    Point.new([-2,3,-2,1]),
+    Point.new([0,2,3,-2]),
+    Point.new([-1,-1,1,-2]),
+    Point.new([0,-2,-1,0]),
+    Point.new([-2,2,3,-1]),
+    Point.new([1,2,2,0]),
+    Point.new([-1,-2,0,-2])
+  ]
+
+  puts(get_num_constellations(example_4_points))
+
+  puts("INPUT SOLUTION:")
+  file_input = get_points_from_file("day25_input.txt")
+  puts(get_num_constellations(file_input))
 end
 
 puts("PART 1 SOLUTIONS:")
 part_1()
-puts("PART 2 SOLUTIONS:")
-part_2()
